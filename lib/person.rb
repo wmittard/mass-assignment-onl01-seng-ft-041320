b4f539278be36307c19d2c54ad9b1b680c449da9
William Mittard <billymittard@ymail.com>
2020-04-29 22:32:07 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,12 @@
 class Person
- attributes = 
+ attributes = {:name, :birthday, :hair_color, :eye_color, :height, 
+:weight, :handed, :complexion, :t_shirt_size, 
+:wrist_size, :glove_size, :pant_length, :pant_width} 
+
+attr_accessor :attributes
+
+def initialize(attributes)
+  @attributes = attributes
+end 
+
 end
